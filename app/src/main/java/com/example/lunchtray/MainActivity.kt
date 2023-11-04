4a928332131413f9ac69355e9192f10f73e85013
Dmytro Siasko <dima.siasko@gmail.com>
2023-11-04 21:00:42 +0200

    Added more order fragments

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lunchtray
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.lunchtray.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 }
